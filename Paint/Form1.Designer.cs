7143452f1ff7065eed86356d5d80c29b4147c167
Maria <razgmaria18vt@gmail.com>
2019-11-01 12:18:57 +0300

    my coment

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.elips = new System.Windows.Forms.Button();
+            this.kvadrat = new System.Windows.Forms.Button();
+            this.pero = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Colors = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel = new System.Windows.Forms.Panel();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Azure;
+            this.panel1.Controls.Add(this.elips);
+            this.panel1.Controls.Add(this.kvadrat);
+            this.panel1.Controls.Add(this.pero);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.Clear);
+            this.panel1.Controls.Add(this.Colors);
+            this.panel1.Location = new System.Drawing.Point(1, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1940, 108);
+            this.panel1.TabIndex = 0;
+            // 
+            // elips
+            // 
+            this.elips.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.elips.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("elips.BackgroundImage")));
+            this.elips.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.elips.Location = new System.Drawing.Point(216, 8);
+            this.elips.Name = "elips";
+            this.elips.Size = new System.Drawing.Size(35, 39);
+            this.elips.TabIndex = 7;
+            this.elips.UseVisualStyleBackColor = true;
+            this.elips.Click += new System.EventHandler(this.elips_Click);
+            // 
+            // kvadrat
+            // 
+            this.kvadrat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.kvadrat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("kvadrat.BackgroundImage")));
+            this.kvadrat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kvadrat.Location = new System.Drawing.Point(173, 8);
+            this.kvadrat.Name = "kvadrat";
+            this.kvadrat.Size = new System.Drawing.Size(35, 39);
+            this.kvadrat.TabIndex = 6;
+            this.kvadrat.UseVisualStyleBackColor = true;
+            this.kvadrat.Click += new System.EventHandler(this.kvadrat_Click);
+            // 
+            // pero
+            // 
+            this.pero.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pero.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pero.BackgroundImage")));
+            this.pero.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pero.Location = new System.Drawing.Point(131, 8);
+            this.pero.Name = "pero";
+            this.pero.Size = new System.Drawing.Size(35, 39);
+            this.pero.TabIndex = 5;
+            this.pero.UseVisualStyleBackColor = true;
+            this.pero.Click += new System.EventHandler(this.pero_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Location = new System.Drawing.Point(378, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Толщина";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(131, 60);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Clear
+            // 
+            this.Clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Clear.Location = new System.Drawing.Point(257, 24);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(108, 23);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Очистка";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Colors
+            // 
+            this.Colors.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Colors.Location = new System.Drawing.Point(257, 57);
+            this.Colors.Name = "Colors";
+            this.Colors.Size = new System.Drawing.Size(115, 23);
+            this.Colors.TabIndex = 0;
+            this.Colors.Text = "Выбор цвета";
+            this.Colors.UseVisualStyleBackColor = true;
+            this.Colors.Click += new System.EventHandler(this.Colors_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem,
+            this.выходToolStripMenuItem,
+            this.выходToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(948, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьКакToolStripMenuItem,
+            this.открытьФайлToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.справкаToolStripMenuItem.Text = "Настройки";
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как..";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // открытьФайлToolStripMenuItem
+            // 
+            this.открытьФайлToolStripMenuItem.Name = "открытьФайлToolStripMenuItem";
+            this.открытьФайлToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.открытьФайлToolStripMenuItem.Text = "Открыть файл..";
+            this.открытьФайлToolStripMenuItem.Click += new System.EventHandler(this.открытьФайлToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.выходToolStripMenuItem.Text = "Справка";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem1
+            // 
+            this.выходToolStripMenuItem1.Name = "выходToolStripMenuItem1";
+            this.выходToolStripMenuItem1.Size = new System.Drawing.Size(53, 20);
+            this.выходToolStripMenuItem1.Text = "Выход";
+            this.выходToolStripMenuItem1.Click += new System.EventHandler(this.выходToolStripMenuItem1_Click);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.statusStrip1);
+            this.panel.Location = new System.Drawing.Point(1, 131);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(2076, 942);
+            this.panel.TabIndex = 2;
+            this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_MouseDown);
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
+            this.panel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel_MouseUp);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 920);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(2076, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(948, 629);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MyPaint";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Colors;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button elips;
+        private System.Windows.Forms.Button kvadrat;
+        private System.Windows.Forms.Button pero;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem открытьФайлToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Threading;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel.CreateGraphics();
+        }
+        /// <summary>
+        /// Текущий цвет
+        /// </summary>
+        Color Color = Color.Black;
+        /// <summary>
+        /// Переменная для опеределения когда можно рисовать на panel
+        /// </summary>
+        bool isPressed = false;
+        /// <summary>
+        /// Текущая точка ресунка
+        /// </summary>
+        Point CurrentPoint;
+        /// <summary>
+        /// Это начальная точка рисунка
+        /// </summary>
+        Point PrevPoint;  
+        /// <summary>
+        /// Графическая переменная
+        /// </summary>
+        Graphics g;
+        /// <summary>
+        /// Диалоговое окно для выбора цвета
+        /// </summary>
+        ColorDialog colorDialog = new ColorDialog();
+        /// <summary>
+        /// Выбор цвета
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Colors_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                Color = colorDialog.Color; //меняем цвет для пера
+                label2.BackColor = colorDialog.Color; //меняем цвет для Фона label2
+            }
+        }
+        /// <summary>
+        /// Очистить холст
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            panel.Refresh();
+            list.Clear();
+            list2.Clear();
+            list3.Clear();
+        }
+        /// <summary>
+        /// Нажали на холст
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPressed = true;
+            if(flag==-1)
+                CurrentPoint = e.Location;
+            if (flag == 0)
+                PrevPoint = e.Location;
+            if (flag == 1)
+                PrevPoint = e.Location;
+        }
+        /// <summary>
+        /// Ведем курсор по холсту
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)//пока жмём рисуем
+            {
+                if (flag == -1) //перо
+                {
+                    PrevPoint = CurrentPoint;
+                    CurrentPoint = e.Location;
+                    paint();
+                }
+            }
+        }
+        /// <summary>
+        /// Переменная для задавания цвета и толщины разным объектам
+        /// </summary>
+        Pen pen;
+        /// <summary>
+        /// Лист, хранящий линии карандаша
+        /// </summary>
+        Karandash list = new Karandash();
+        /// <summary>
+        /// Лист, хранящий прямоугольники
+        /// </summary>
+        Paint.Pryam list2 = new Paint.Pryam();
+        /// <summary>
+        /// Лист, хранящий эллипсы
+        /// </summary>
+        Paint.Pryam list3 = new Paint.Pryam();
+        
+        /// <summary>
+        /// Метод, рисующий карандашом
+        /// </summary>
+        private void paint()
+        {
+            fl = false;
+            pen = new Pen(Color, (float)numericUpDown1.Value); //Создаем перо, задаем ему цвет и толщину.
+            pen.StartCap = LineCap.Round;
+            pen.EndCap = LineCap.Round;
+            pen.LineJoin = LineJoin.Round;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            list.Add(pen.Color, (int)pen.Width, CurrentPoint, PrevPoint);
+            g.DrawLine(pen, CurrentPoint, PrevPoint);
+
+        }
+
+        /// <summary>
+        /// Метод, рисующий прямоугольники и эллипсы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+            fl = false;
+            if (flag == -1) // если рисуем карандашом, уходим из метода
+                return;
+            CurrentPoint = e.Location;
+            pen = new Pen(Color, (float)numericUpDown1.Value);
+            int width = CurrentPoint.X - PrevPoint.X; // запоминание координат
+            int heidht = CurrentPoint.Y - PrevPoint.Y; // запоминание координат
+            if (width < 0 && heidht < 0) // Перенос ключевых точек для рисования
+            {
+                Point t = CurrentPoint;
+                CurrentPoint = PrevPoint;
+                PrevPoint = t;
+            }
+            else if (width > 0 && heidht < 0) // Перенос ключевых точек для рисования
+            {
+                Point def1 = PrevPoint;
+                Point def2 = CurrentPoint;
+                PrevPoint.X = def1.X;
+                PrevPoint.Y = def2.Y;
+                CurrentPoint.X = def2.X;
+                CurrentPoint.Y = def1.Y;
+
+            }
+            else if (width < 0 && heidht > 0) // Перенос ключевых точек для рисования
+            {
+                Point def1 = PrevPoint;
+                Point def2 = CurrentPoint;
+                PrevPoint.X = def2.X;
+                PrevPoint.Y = def1.Y;
+                CurrentPoint.X = def1.X;
+                CurrentPoint.Y = def2.Y;
+            }
+            if (flag == 0) // рисуем прямоугольник
+            {
+                g.DrawRectangle(pen, PrevPoint.X, PrevPoint.Y, CurrentPoint.X - PrevPoint.X, CurrentPoint.Y - PrevPoint.Y);
+                list2.Add(pen.Color, (int)pen.Width, PrevPoint,CurrentPoint);
+            }
+            if(flag == 1) // рисуем эллипс
+            {
+                g.DrawEllipse(pen, PrevPoint.X, PrevPoint.Y, CurrentPoint.X - PrevPoint.X, CurrentPoint.Y - PrevPoint.Y);
+                list3.Add(pen.Color,(int)pen.Width, PrevPoint, CurrentPoint);
+            }
+        }
+
+        private void выходToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Выйти ?", "Вопрос", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Версия 10.1705.21204.0 \n © Корпорация Весёлый Чув(Veselyi Tchuv Corporation), 2019.\n Все права защищены.\n", "Справка", MessageBoxButtons.OK);
+
+        }
+
+        /// <summary>
+        /// Метод, запускающийся при изменении размеров формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            Ref();
+        }
+        /// <summary>
+        /// Флаг для определения когда отрисовывать листы, а когда файл из загрузки
+        /// </summary>
+        bool fl = false;
+        /// <summary>
+        /// Перерисовка объектов
+        /// </summary>
+        private void Ref()
+        {
+            Pen p;
+            if (fl)
+            {
+                list = inst.Add1;
+                list2 = (Paint.Pryam)inst.Add2;
+                list3 = (Paint.Pryam)inst.Add3;
+            }
+            if (list.Count!=0)
+            {
+                Element1 t = list.Head;
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Color col = Color.FromArgb(t.Col);
+                    p = new Pen(col, t.T);
+                    p.StartCap = LineCap.Round;
+                    p.EndCap = LineCap.Round;
+                    p.LineJoin = LineJoin.Round;
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+                    g.DrawLine(p,t.X, t.Y);
+                    t = t.Next;
+                }
+            }
+            if (list2.Count != 0)
+            {
+                Element1 t = list2.Head;
+                for (int i = 0; i < list2.Count; i++)
+                {
+                    Color col = Color.FromArgb(t.Col);
+                    p = new Pen(col, t.T);
+                    g.DrawRectangle(p, t.X.X,t.X.Y,t.Y.X- t.X.X, t.Y.Y- t.X.Y);
+                    t = t.Next;
+                }
+            }
+            if (list3.Count != 0)
+            {
+                Element1 t = list3.Head;
+                for (int i = 0; i < list3.Count; i++)
+                {
+                    Color col = Color.FromArgb(t.Col);
+                    p = new Pen(col, t.T);
+                    g.DrawEllipse(p, t.X.X, t.X.Y, t.Y.X - t.X.X, t.Y.Y - t.X.Y);
+                    t = t.Next;
+                }
+            }
+
+        }
+        /// <summary>
+        /// Флаг для определения, чем сейчас рисуют (-1 - "Карандаш",0 - "Прямоугольник", 1 - "Эллипс")
+        /// </summary>
+        private int flag = -1;
+        /// <summary>
+        /// Выбрали карандаш
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pero_Click(object sender, EventArgs e)
+        {
+            flag = -1;
+        }
+        /// <summary>
+        /// Выбрали прямоугольник
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void kvadrat_Click(object sender, EventArgs e)
+        {
+            flag = 0;
+        }
+
+        /// <summary>
+        /// Выбрали эллипс
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void elips_Click(object sender, EventArgs e)
+        {
+            flag = 1;
+        }
+        XmlSerializer formatter = new XmlSerializer(typeof(Instrument));
+        Instrument inst = new Instrument();
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                inst.Add1= list;
+                inst.Add2 = list2;
+                inst.Add3 = list3;
+                string name = saveFileDialog1.FileName;
+                if (name.IndexOf('.') != -1)
+                {
+                    name = name.Remove(name.IndexOf('.'), 4);
+                }
+                using (FileStream fs = new FileStream(name + ".xml", FileMode.OpenOrCreate))
+                {
+                    statusStrip1.Show();
+                    formatter.Serialize(fs, inst);
+                }
+            }
+        }
+
+        private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string name = openFileDialog1.FileName;
+                    string xml = name.Remove(0, name.Length - 3);
+                    if (xml != "xml")
+                        throw new Exception("Неверный формат");
+                    using (FileStream fs = new FileStream(name, FileMode.Open))
+                    {
+                        list.Clear();
+                        list2.Clear();
+                        list3.Clear();
+                        panel.Refresh();
+                        inst = (Instrument)formatter.Deserialize(fs);
+                        fl = true;
+                        Ref();
+                    }
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+    }
+}
